321b8b64a3eb6b706a4a0953a4422291826af09f
koladeade <k.adepoju@alustudent.com>
2024-10-19 11:26:42 -0700

    task2

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
-"""Script to use a REST API, returns information about
-all tasks from all employees and export in JSON"""
+"""Script to use a REST API for a given employee ID, returns
+information about his/her TODO list progress and export in JSON"""
 import json
 import requests
+import sys
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"UsageError: python3 {__file__} employee_id(int)")
+        sys.exit(1)
+
     API_URL = "https://jsonplaceholder.typicode.com"
+    EMPLOYEE_ID = sys.argv[1]
 
-    users = requests.get(f"{API_URL}/users").json()
+    response = requests.get(
+        f"{API_URL}/users/{EMPLOYEE_ID}/todos",
+        params={"_expand": "user"}
+    )
+    data = response.json()
 
-    dict_users_tasks = {}
-    for user in users:
-        tasks = requests.get(f"{API_URL}/users/{user['id']}/todos").json()
+    if not len(data):
+        print("RequestError:", 404)
+        sys.exit(1)
 
-        dict_users_tasks[user["id"]] = []
-        for task in tasks:
-            task_dict = {
-                "username": user["username"],
-                "task": task["title"],
-                "completed": task["completed"]
-            }
-            dict_users_tasks[user["id"]].append(task_dict)
+    user_tasks = {EMPLOYEE_ID: []}
+    for task in data:
+        task_dict = {
+            "task": task["title"],
+            "completed": task["completed"],
+            "username": task["user"]["username"]
+        }
+        user_tasks[EMPLOYEE_ID].append(task_dict)
 
-    with open("todo_all_employees.json", "w") as file:
-        json.dump(dict_users_tasks, file)
+    with open(f"{EMPLOYEE_ID}.json", "w") as file:
+        json.dump(user_tasks, file)
